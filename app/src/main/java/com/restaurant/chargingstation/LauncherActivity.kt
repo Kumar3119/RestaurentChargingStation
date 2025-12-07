c888076147741bfc4a8b4d3200df334e7ad6c352
Kumar Love Chawada <lovechawada31@Kumars-MacBook-Pro.local>
2025-12-07 15:23:08 +0530

    final code

--- app/src/main/java/com/restaurant/chargingstation/LauncherActivity.kt
+++ app/src/main/java/com/restaurant/chargingstation/LauncherActivity.kt
@@ -22,6 +22,7 @@ class LauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         // Start the core application flow
         ApplicationController.start()
 
